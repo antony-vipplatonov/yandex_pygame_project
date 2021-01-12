21d6d1a74e9344e3303d22efcd3e784a538c9eb5
Anton <antony-vipplatonov@yandex.ru>
2021-01-12 17:46:15 +0300

    board changed

--- Pac-Man.py
+++ Pac-Man.py
@@ -1,35 +1,70 @@
 import pygame
+import os
+import sys
 
 
 class Board:
     def __init__(self, level):
         self.level = level
-        self.cell_size = 30
 
     def render(self, screen):
+        decorations = pygame.sprite.Group()
+        points = pygame.sprite.Group()
         for i in range(len(self.level)):
             for j in range(len(self.level[i])):
                 if self.level[i][j] == '#':
-                    pygame.draw.rect(screen, pygame.Color('blue'),
-                                     ((25 + j * 30, 25 + i * 30), (30, 30)), 0)
+                    sprite = pygame.sprite.Sprite()
+                    sprite.image = self.load_image("wall.jpg")
+                    sprite.rect = sprite.image.get_rect()
+                    sprite.rect.x = 25 + j * 30
+                    sprite.rect.y = 25 + i * 30
+                    decorations.add(sprite)
                 elif self.level[i][j] == '-':
-                    pygame.draw.rect(screen, pygame.Color('brown'),
-                                     ((25 + j * 30, 25 + i * 30), (30, 10)), 0)
+                    sprite = pygame.sprite.Sprite()
+                    sprite.image = self.load_image("door.jpg")
+                    sprite.rect = sprite.image.get_rect()
+                    sprite.rect.x = 25 + j * 30
+                    sprite.rect.y = 25 + i * 30
+                    decorations.add(sprite)
                 elif self.level[i][j] == '.':
-                    pygame.draw.circle(screen, pygame.Color('pink'),
-                                       (40 + j * 30, 40 + i * 30), 3)
+                    sprite = pygame.sprite.Sprite()
+                    sprite.image = self.load_image("small_point.jpg")
+                    sprite.rect = sprite.image.get_rect()
+                    sprite.rect.x = 32 + j * 30
+                    sprite.rect.y = 32 + i * 30
+                    points.add(sprite)
                 elif self.level[i][j] == ',':
-                    pygame.draw.circle(screen, pygame.Color('pink'),
-                                       (40 + j * 30, 40 + i * 30), 7)
+                    sprite = pygame.sprite.Sprite()
+                    sprite.image = self.load_image("big_point.jpg")
+                    sprite.rect = sprite.image.get_rect()
+                    sprite.rect.x = 27 + j * 30
+                    sprite.rect.y = 27 + i * 30
+                    points.add(sprite)
+        decorations.draw(screen_play)
+        points.draw(screen_play)
 
     def find_cell(self, pos):
-        '''когда пакмен будет находиться на клетке с точкой,то сюда будут передоваться его координаты, чтобы определить положение клетки и по ней удалить её из self.level'''
         x, y = pos[0], pos[1]
         for i in range(len(self.level)):
             for j in range(len(self.level[i])):
                 if x > 25 + j * 30 and x < 25 + (j + 1) * 30 and \
                         y > 25 + i * 30 and y < 25 + (i + 1) * 30:
                     return i, j
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('images', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
 
 
 if __name__ == '__main__':
